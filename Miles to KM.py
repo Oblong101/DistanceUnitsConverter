8731dfb6555a083494ac58e6ab31bee174e10989
Oblong101 <121264821+Oblong101@users.noreply.github.com>
2022-12-28 23:57:43 +0000

    Update Miles to KM.py

--- Miles to KM.py
+++ Miles to KM.py
@@ -1,10 +1,12 @@
 #A simple program to convert miles to kilometres and vice versa
 #Currently you can only convert km to miles by doing miles to km first
 
-mi = int(input("Input your distance in miles: "))
+mi = float(input("Input your distance in miles: "))
 kilom = mi*1.609
 print("Miles to KM:" , kilom)
-print()
-km = int(input("Input your distance in KM: "))
+
+print() #leaves a space between km and m calculations
+
+km = float(input("Input your distance in KM: "))
 miles = km*0.609
 print("KM to Miles:" , miles)
