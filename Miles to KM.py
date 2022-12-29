4b8591a190d67e2cd79b35651348e59d3df89215
Oblong101 <121264821+Oblong101@users.noreply.github.com>
2022-12-29 04:10:54 +0000

    Update Miles to KM.py

--- Miles to KM.py
+++ Miles to KM.py
@@ -2,12 +2,12 @@ x = input("Km to M(KTM) or M to Km(MTK)? ")
 if x == "KTM":
     km1 = float(input("Input the distance in Km: "))
     miles1 = km1*1.609
-    print(km1, "in miles is", miles1)
+    print(km1, "Kilometres in miles is", miles1, "Miles")
 
 if x == "MTK":
     miles2 = float(input("Input the distance in M: "))
     km2 = miles2*0.609
-    print(miles2, "in Km is", km2)
+    print(miles2, "Miles in Km is", km2, "Kilometres")
 
 else:
     print("Error. Calculation must be either MTK(miles to kilometres) or KTM(kilometres to miles)")
